d5fbd0e11d8d066928260e0ae6f50a3ccb73dbb0
James Hunt <james@niftylogic.com>
2018-07-13 11:42:03 -0400

    Death to Hugo!

--- hugo/content/download.md
+++
@@ -1,7 +0,0 @@
----
-title: SHIELD Releases
-date: 2017-11-09T16:09:33-05:00
-layout: download
----
-This page is dummy to get hugo to generate a page.
-See themes/shield/layouts/page/downloads for the real source.
--- hugo/themes/shield/static/libs/jquery/table_of_contents.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(e) {
-    $(".topmenu").on('click',function (e) {
-        $(this).children('div').children('ul').toggle('');
-        e.preventDefault()
-    });
-    $('.super > li div ul li a').click(function(e) {
-        e.stopPropagation();
-    });
-});
