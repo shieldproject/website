85f6d16f23bc35cf6f555b1b203c8032ff51bdae
James Hunt <james@niftylogic.com>
2018-08-10 08:30:51 -0400

    Anchors on H1-H4

--- htdocs/js/common.js
+++ htdocs/js/common.js
@@ -737,8 +737,9 @@
 
 
   if ($('.content').length > 0) {
-    var headings = $('.content').find('h1,h2'),
-        main = $('<ul>'),
+    var headings = $('.content').find('h1,h2,h3,h4'),
+           stack = [],
+            main = $('<ul>'),
        outer, inner, li;
 
     headings.each(function (i,e) {
@@ -752,16 +753,28 @@
                      .replace(/[^a-z0-9-]+/g, '-')
                      .replace(/^-/g, '')
                      .replace(/-$/g, '')
-
-        $(e).attr('id', anchor);
       }
+
+      if (type == 'H2') {
+        stack = [anchor];
+      } else if (type == 'H3') {
+        stack.push(anchor);
+        anchor = stack.join(':');
+      } else if (type == 'H4') {
+        stack.push(anchor);
+        anchor = stack.join(':');
+        stack.pop();
+      }
+      $(e).attr('id', anchor);
+
+      $(e).prepend('<a href="#'+anchor+'"><svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg></a>');
 
       li = $('<li><a href="#'+anchor+'">'+text+'</a></li>');
       if (type == 'H1') {
         outer = $(li);
         main.append(outer);
         inner = undefined;
-      } else {
+      } else if (type == 'H2') {
         if (typeof(inner) === 'undefined') {
           inner = $('<ul>');
           outer.append(inner);
